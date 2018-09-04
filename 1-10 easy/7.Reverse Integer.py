2add21aacb8313ca5f4afcb8f8b9a05794fc3a0c
AndFreedoom <[liuyingxinwy@163.com]>
2018-09-04 17:35:25 +0800

    1

--- 1-10 easy/7.Reverse Integer.py
+++ 1-10 easy/7.Reverse Integer.py
@@ -4,7 +4,7 @@ def reverseint(x):
         new_x = -1 * int(str(-x)[::-1])
     else :
         new_x = int(str(x)[::-1])
-    if new_x > 2**31 and new_x < (-(2**31)-1):
+    if new_x > 2**31-1 or new_x < -(2**31):
         new_x = 0
     return new_x
 
@@ -25,7 +25,8 @@ def reverseint(x):
         new_x = 10 * new_x + x % 10
         x = x // 10
     new_x = flag * new_x
-    if new_x > 2**31 and new_x < (-(2**31)-1):
+    if new_x > (2**31-1) or new_x < -(2**31):
         new_x = 0
     return new_x
+
 
